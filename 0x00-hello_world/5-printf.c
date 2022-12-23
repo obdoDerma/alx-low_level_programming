a064966625f5d4eef10371d0b1925e467d0fb6b2
obdoDerma <obdo_hack229@yahoo.com>
2022-12-23 01:43:37 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,5 +7,5 @@
  */
 int main(void)
 {
-	printg("with proper grammer, but the outcome is a piece of art,\n")		return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n")		return (0);
 }
