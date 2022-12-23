029a928800b5905d92667fe36094b1b6fa803115
obdoDerma <obdo_hack229@yahoo.com>
2022-12-23 01:58:49 -0800

    tasl 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,14 +8,10 @@
  *
  *    *  Return: 0
  *
- *     */
-
+ **/
 int main(void)
 
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
