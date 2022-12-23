6e1c8e378968686198e53d5fbb36676cc062a642
obdoDerma <obdo_hack229@yahoo.com>
2022-12-23 02:19:18 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,9 @@
 
 /**
  *
- *    main - main block
- *
- *
- *      Return: 0
+ *    main -prints the phrase with proper grammar, 
+ *    but the outcome is a piece of art,
+ *      Return: 0 if exited properly, non-zero otherwise
  *
  **/
 int main(void)
