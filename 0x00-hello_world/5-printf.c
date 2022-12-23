155dfcb4fb83cfb0c7e20b76bedea008d685bc47
obdoDerma <obdo_hack229@yahoo.com>
2022-12-23 01:23:31 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include	<stdio.h>
+
+/**
+ * 	main	-	prints	the	phrase	with	proper	grammer,
+ * 	but	the	outcome	is a piece of art,
+ * 	Return: 0 if exited properly, non-zero otherwise
+ */
+int	main(void)
+{
+	printg("with proper grammer, but the outcome is a pice if art,\n")
+	return	(0);
+}
