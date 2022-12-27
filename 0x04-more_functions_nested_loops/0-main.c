7f90e2c219fed45ff879feee72ee7090380aa109
obdoDerma <obdo_hack229@yahoo.com>
2022-12-27 08:03:02 -0800

    0-isupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
 #include <stdio.h>
 
 /**
- *	main - check the code.
- *	Return: Always 0.
+ *main - check the code.
+ *Return: Always 0.
  */
-
 int main(void)
 
 {
-
 	char c;
 
 	c = 'A';
