f8f41e0806135486132c8fd6fba8be9963062300
obdoDerma <obdo_hack229@yahoo.com>
2022-12-27 06:41:20 -0800

    convert

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,10 @@
 #include "main.h"
-
 /**
- * main - Test function for positive or negative
+ * main - test function thay prints if positive or negative
  * Return: 0
  */
-
 int main(void)
-
 {
-
 	int i;
 
 	i = 0;
